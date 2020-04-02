a2cbdd49536ae2b37172767ef4a70e5e5a2374d3
Sujay Patil <sujaysanjeev.patil@gmail.com>
2020-04-02 12:08:07 -0700

    modified JOIN criteria

--- REDCap_HealthPro_Sync/main.py
+++ REDCap_HealthPro_Sync/main.py
@@ -21,47 +21,49 @@ if __name__ == "__main__":
 	# process at USC's Keck hospital (AoU records)
 	rc_recs = export_redcap_records(URL, REDCAP_API_KEY)
 
+	# convert the `last_name` column in both REDCap and HealthPro records to lowercase
+	# the reason is because JOIN is case-sensitive
+	hp_recs['last_name'] = hp_recs['last_name'].str.lower()
+	rc_recs['last_name'] = rc_recs['last_name'].str.lower()
+
 	# create a LEFT JOIN between HealthPro records and exisiting AoU records
-	# columns on which we have performed the JOIN are `pmi_id` and `dob`
-	hp_rc_merged = pd.merge(hp_recs, rc_recs, left_on=['pmi_id', 'dob', 'physical_measurements_complete_date'], 
-											  right_on=['pmi_id', 'dob', 'gift_card_crc_sign_date'], 
+	# columns on which we have performed the JOIN are `pmi_id`, `dob`, `last_name`
+	hp_rc_merged = pd.merge(hp_recs, rc_recs, left_on=['pmi_id', 'dob', 'last_name'], 
+											  right_on=['pmi_id', 'dob', 'last_name'], 
 											  how='left')
-	print(hp_rc_merged)
 
 	# to count the number of records which have not been assigned any 'study_id' (study_id == NaN)
 	unassigned_recs = len(hp_rc_merged) - hp_rc_merged['study_id'].count()
 
 	print("No. of records which could not be matched with a `study_id`: " + str(unassigned_recs))
 
-	# study_id's that need to be manually resolved because there are no matching columns while
-	# creating JOIN
-	exclude = ['162890', '19209', '87121']
-	hp_rc_merged = hp_rc_merged[~hp_rc_merged['study_id'].isin(exclude)]
-
 	# identify duplicates which still remain after the matching
-	hp_rc_merged_dups = hp_rc_merged[hp_rc_merged.duplicated(subset=['pmi_id', 'dob', 'gift_card_crc_sign_date'], keep=False)]
-	print(hp_rc_merged_dups)
+	# hp_rc_merged_dups = hp_rc_merged[hp_rc_merged.duplicated(subset=['pmi_id', 'dob', 'gift_card_crc_sign_date'], keep=False)]
+	# print(hp_rc_merged_dups)
 
 	# prepare dataframe from those records which have a matching `study_id`
 	# i.e., ones without NaN in their study_id column
 	hp_rc_not_nulls = hp_rc_merged[hp_rc_merged['study_id'].notnull()]
+
 	hp_rc_not_nulls.columns = ['hp_' + str(col) if col != "study_id" else str(col) for col in hp_rc_not_nulls.columns]
+	hp_rc_not_nulls['hp_last_name'] = hp_rc_not_nulls['hp_last_name'].str.capitalize()
 	print(hp_rc_not_nulls)
-	# hp_rc_not_nulls.to_csv('records_matched.csv', index=False)
+	# hp_rc_not_nulls.to_csv('records_matched_pmi_id.csv', index=False)
 
 	# prepare dataframe from those records which have NaN value in their study_id column
 	hp_rc_nulls = hp_rc_merged[hp_rc_merged['study_id'].isna()]
 
 	# compute maximum value of study_id that is present among the records in exisiting AoU instance/project
 	max_study_id = rc_recs['study_id'].apply(pd.to_numeric).max()
-	print("Maximum study id is: " + str(max_study_id))
+	# print("Maximum study id is: " + str(max_study_id))
 
 	# increment the study_id so we can assign values to records with missing study_id's
 	max_study_id = max_study_id + 1
 	hp_rc_nulls['study_id'] = max_study_id + range(len(hp_rc_nulls))
 	hp_rc_nulls.columns = ['hp_' + str(col) if col != "study_id" else str(col) for col in hp_rc_nulls.columns]
+	hp_rc_nulls['hp_last_name'] = hp_rc_nulls['hp_last_name'].str.capitalize()
 	print(hp_rc_nulls)
-	# hp_rc_nulls.to_csv('nulls.csv', index=False)
+	# hp_rc_nulls.to_csv('nulls_pmi_id.csv', index=False)
 
 	not_null_dicts = []	# list of dicts that will represent the records in the hp_rc_not_nulls dataframe
 	not_null_dicts = hp_rc_not_nulls.to_dict('records')
